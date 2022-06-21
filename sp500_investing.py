e69c63c0d6d9f3daf9699a2709732c9d832ea495
James <jamesmontyn@gmail.com>
2022-06-21 16:53:46 +0200

    testing with monthly buying at last day or 2pct drop

--- sp500_investing.py
+++ sp500_investing.py
@@ -1,4 +1,9 @@
+import datetime as dt
+from datetime import datetime
+from lib2to3.pgen2.token import NEWLINE
+from telnetlib import BM
 import pandas as pd
+from pandas.tseries.offsets import BMonthEnd
 import yfinance as yf
 from yahoofinancials import YahooFinancials
 
@@ -8,26 +13,78 @@ spy_df = yf.download('SPY',
                       rounding=False, 
                       progress=False)
 
+
 print(spy_df)
 
-
-#Calculating percentages change per day
+#Percentages changes per day
 change_open_to_close = []           #Pct change between Open and Close
 change_open_to_low  = []            #Pct change between Open and Low
 change_prevclose_to_close  = []     #Pct change between Close of previous day and Close
 change_prevclose_to_low  = []       #Pct change between Close of previous day and Low
 PrevClose = spy_df['Close'][0]
 
-for ind in spy_df.index:
-    change_open_to_close.append(((spy_df['Close'][ind] - spy_df['Open'][ind]) / spy_df['Open'][ind]) * 100)
-    change_open_to_low.append(((spy_df['Low'][ind] - spy_df['Open'][ind]) / spy_df['Open'][ind]) * 100)
-    change_prevclose_to_close.append(((spy_df['Close'][ind] - PrevClose) / PrevClose) * 100)
-    change_prevclose_to_low.append(((spy_df['Low'][ind] - PrevClose) / PrevClose) * 100)
-    PrevClose = spy_df['Close'][ind]
+for date in spy_df.index:
+    change_open_to_close.append(((spy_df['Close'][date] - spy_df['Open'][date]) / spy_df['Open'][date]) * 100)
+    change_open_to_low.append(((spy_df['Low'][date] - spy_df['Open'][date]) / spy_df['Open'][date]) * 100)
+    change_prevclose_to_close.append(((spy_df['Close'][date] - PrevClose) / PrevClose) * 100)
+    change_prevclose_to_low.append(((spy_df['Low'][date] - PrevClose) / PrevClose) * 100)
+    PrevClose = spy_df['Close'][date]
 
-spy_df['Pct. Open to Low'] = change_open_to_close
-spy_df['Pct. Open to Close'] = change_open_to_low
-spy_df['Pct. Prev. Close to Close'] = change_prevclose_to_close
-spy_df['Pct. Prev. Close to Low'] = change_prevclose_to_low
+#Results dataframe
+results_df = pd.DataFrame(index=spy_df.index)
+lastBuyMonth = 1
+prevDate = datetime(1993, 1, 29)                       #starting with 1993-02-01, previous date was 1993-01-29
+budgetPerMonth = 100
+totalDeposit = 0
+avgBuyPrice = 0
 
-print(spy_df)
+#Buy monthly at a drop of 2 pct. between open to close, or last day of month
+avgprice_monthly_drop_2pct = [0]              #Average share price of investment
+shares_monthly_drop_2pct = [0]                #Number of shares
+
+results_df['Pct. Open to Low'] = change_open_to_close
+results_df['Pct. Open to Close'] = change_open_to_low
+results_df['Pct. Prev. Close to Close'] = change_prevclose_to_close
+results_df['Pct. Prev. Close to Low'] = change_prevclose_to_low
+
+for date in results_df.index:
+    if lastBuyMonth is not date.month:
+        if (date.month - lastBuyMonth) == 2 or (date.month - lastBuyMonth) == -10:
+            #buy close of last day of previous month
+            noSharesBuying = budgetPerMonth / spy_df['Close'][prevDate]
+            newNoShares = shares_monthly_drop_2pct[-1] + noSharesBuying
+            avgprice_monthly_drop_2pct[-1] = ((avgprice_monthly_drop_2pct[-1] * shares_monthly_drop_2pct[-1] 
+                                               + spy_df['Close'][prevDate] * noSharesBuying) / newNoShares)
+            shares_monthly_drop_2pct[-1] = (newNoShares)
+            lastBuyMonth = prevDate.month
+            totalDeposit += budgetPerMonth
+
+        if results_df['Pct. Open to Close'][date] <= -2:
+            print(date, spy_df['Close'][date], results_df['Pct. Open to Close'][date])
+            #buy close of current date
+            noSharesBuying = (budgetPerMonth * 5) / spy_df['Close'][date]
+            newNoShares = shares_monthly_drop_2pct[-1] + noSharesBuying
+            avgprice_monthly_drop_2pct.append((avgprice_monthly_drop_2pct[-1] * shares_monthly_drop_2pct[-1] 
+                                               + spy_df['Close'][date] * noSharesBuying) / newNoShares)
+            shares_monthly_drop_2pct.append(newNoShares)
+            lastBuyMonth = date.month
+            totalDeposit += budgetPerMonth * 5
+            continue
+
+    avgprice_monthly_drop_2pct.append(avgprice_monthly_drop_2pct[-1])
+    shares_monthly_drop_2pct.append(shares_monthly_drop_2pct[-1])
+    prevDate = date
+
+#Remove the initial 0's from the lists
+avgprice_monthly_drop_2pct.remove(0)
+shares_monthly_drop_2pct.remove(0)
+
+results_df['Avg. price (monthly & 2pct.)'] = avgprice_monthly_drop_2pct
+results_df['Shares (monthly & 2pct.)'] = shares_monthly_drop_2pct
+
+print(results_df)
+print("total deposit:", totalDeposit)
+print("total value:", spy_df['Close'][-1] *  results_df['Shares (monthly & 2pct.)'][-1])
+print("pct. profit:", ((spy_df['Close'][-1] *  results_df['Shares (monthly & 2pct.)'][-1] - totalDeposit) / totalDeposit) * 100)
+
+
